0440a806372422eb23f3e141b712e5dfa0bcc9ff
Ashaverus <kaykymdlima@gmail.com>
2025-12-24 10:19:07 -0300

    Update Cliente.java

--- src/Cliente.java
+++ src/Cliente.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 public class Cliente extends Pessoa {
     private ArrayList<Ingresso> ingressosComprados;
 
+    public ArrayList<Ingresso> getIngressosComprados() {
+    return ingressosComprados;
+    }
     public Cliente(String nome, String cpf, String email) {
         super(nome, cpf, email);
         ingressosComprados = new ArrayList<>();

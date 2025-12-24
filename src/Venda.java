cbfc880ba4914607f1552335346018c7485735c4
Ashaverus <kaykymdlima@gmail.com>
2025-12-24 10:18:57 -0300

    Update Venda.java

--- src/Venda.java
+++ src/Venda.java
@@ -1,2 +1,10 @@
+import java.util.ArrayList;
+
 public class Venda {
+    public void realizarVenda(Cliente cli, Ingresso ing) {
+        cli.getIngressosComprados().add(ing);
+    }
+    public void realizarVenda(Cliente cli, ArrayList<Ingresso> ing){
+        cli.getIngressosComprados().addAll(ing);
+    }
 }

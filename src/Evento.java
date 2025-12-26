b0b95341d21991ccc1c2019f522d9ff8eb630460
Ashaverus <kaykymdlima@gmail.com>
2025-12-26 19:46:31 -0300

    Update Evento.java

--- src/Evento.java
+++ src/Evento.java
@@ -1,16 +1,14 @@
-import java.util.ArrayList;
+
 
 public class Evento {
     private String nome;
     private String local;
     private String data;
-    private ArrayList<Ingresso> ingressos;
 
     public Evento(String nome, String local, String data){
         this.nome = nome;
         this.local = local;
         this.data = data;
-        this.ingressos = new ArrayList<>();
     }
 
     public String getNome() {
